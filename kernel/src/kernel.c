fee3ec337cf6a2df68e9ef54ab4d2cfae5184afa
Ian <spurious.vector@gmail.com>
2022-08-05 16:53:21 -0400

    common

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,10 +2,6 @@
 #include <stddef.h>
 #include <limine.h>
 
-static volatile struct limine_terminal_request terminal_request = {
-    .id = LIMINE_TERMINAL_REQUEST,
-    .revision = 0
-};
 
 static void done(void) {
     for (;;) {
@@ -14,13 +10,5 @@ static void done(void) {
 }
 
 void _start(void) {
-    if (terminal_request.response == NULL
-     || terminal_request.response->terminal_count < 1) {
-        done();
-    }
-
-    struct limine_terminal *terminal = terminal_request.response->terminals[0];
-    terminal_request.response->write(terminal, "Hello World", 11);
-
     done();
 }
