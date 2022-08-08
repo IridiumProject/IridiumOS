e4c56fdcb1719ba5a0a5cd3980ee6d31a485906b
Ian Moffett <spurious.vector@gmail.com>
2022-08-08 19:24:42 -0400

    Misc: Make status colorful.

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -6,6 +6,10 @@
 #include <mm/pmm.h>
 #include <mm/vmm.h>
 
+#define STATUS_MSG_COLOR CYAN
+#define STATUS_MSG_OK_COLOR KINFO
+
+
 static void done(void) {
     for (;;) {
         __asm__("hlt");
@@ -14,10 +18,11 @@ static void done(void) {
 
 void _start(void) {
     intr_init();
-    kprintf(KINFO "[Interrupts Init]: OK\n");
+    kprintf(STATUS_MSG_COLOR "[Interrupts Init]:" STATUS_MSG_OK_COLOR " OK\n");
     pmm_init();
-    kprintf("[PMM]: OK\n");
+    kprintf(STATUS_MSG_COLOR "[PMM]:" STATUS_MSG_OK_COLOR " OK\n");
     vmm_init();
-    kprintf("[VMM]: OK\n");
+    kprintf(STATUS_MSG_COLOR "[VMM]:" STATUS_MSG_OK_COLOR " OK\n");
+	kprintf(STATUS_MSG_COLOR "[ACPI]:" STATUS_MSG_OK_COLOR " OK\n");
     done();
 }
