6eaa5972532dada1d8da2500923a0317d525b8ec
Ian Moffett <spurious.vector@gmail.com>
2022-08-10 18:07:54 -0400

    Misc: Update kernel.c

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,12 +2,14 @@
 #include <stddef.h>
 #include <limine.h>
 #include <common/log.h>
+#include <common/asm.h>
 #include <intr/intr.h>
 #include <mm/pmm.h>
 #include <mm/vmm.h>
 #include <firmware/acpi/acpi.h>
 #include <arch/x86/ioapic.h>
 #include <arch/x86/lapic.h>
+#include <timer/pit.h>
 
 static void done(void) {
     for (;;) {
@@ -15,7 +17,8 @@ static void done(void) {
     }
 }
 
-void _start(void) {
+
+static void init(void) {
     intr_init();
     kprintf(STATUS_MSG_COLOR "[Interrupts Init]:" STATUS_MSG_OK_COLOR " OK\n");
     pmm_init();
@@ -28,5 +31,14 @@ void _start(void) {
 	kprintf(STATUS_MSG_COLOR "[I/O APIC]:" STATUS_MSG_OK_COLOR " OK\n");
 	lapic_init();
 	kprintf(STATUS_MSG_COLOR "[Local APIC]:" STATUS_MSG_OK_COLOR " OK\n");
+	pit_init();
+	kprintf(STATUS_MSG_COLOR "[PIT]:" STATUS_MSG_OK_COLOR " OK\n");
+	init_irqs();
+	kprintf(STATUS_MSG_COLOR "[IRQS]:" STATUS_MSG_OK_COLOR " OK\n");
+	STI;
     done();
+}
+
+void _start(void) {
+	init();
 }
