15b32c1c872fb21104ae2649e70fe082ab3ac30d
Ian Moffett <spurious.vector@gmail.com>
2022-08-10 20:01:14 -0400

    Update irq.asm

--- kernel/src/asm/intr/irq.asm
+++ kernel/src/asm/intr/irq.asm
@@ -1,10 +1,15 @@
 bits 64
 global irq0_isr
+global pit_ticks
 
 extern lapic_send_eoi
 
 irq0_isr:
 	cli
+	inc dword [pit_ticks]
 	call lapic_send_eoi
 	sti
 	iretq
+
+section .data
+pit_ticks: dw 0
