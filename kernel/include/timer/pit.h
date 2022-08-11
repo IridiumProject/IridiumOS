77ca4bb2539252b44b1464df6a817d3fd814b74b
Ian Moffett <spurious.vector@gmail.com>
2022-08-10 20:00:59 -0400

    Feat: Sleep.

--- kernel/include/timer/pit.h
+++ kernel/include/timer/pit.h
@@ -1,9 +1,16 @@
 #ifndef PIT_H
 #define PIT_H
 
+#include <stdint.h>
+
 #define PIT_FREQUENCY_HZ 100
 
-void pit_init(void);
+// S prefix means safe.
+#define S_PIT_SLEEP(n_ticks) __asm__ __volatile__("cli"); \
+							 pit_sleep(n_ticks);          
+							 // STI is called before the end of pit_sleep()
 
+void pit_init(void);
+void pit_sleep(uint16_t n_ticks);
 
 #endif
