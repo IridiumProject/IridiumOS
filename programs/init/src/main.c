1d1917a6bc3884e730fc6cf96f9e0286a600c641
Ian Moffett <spurious.vector@gmail.com>
2022-08-31 14:16:07 -0400

    Feat: message on keystroke.

--- programs/init/src/main.c
+++ programs/init/src/main.c
@@ -1,5 +1,19 @@
+#include <stdint.h>
+
 int main(void) {
-    __asm__ __volatile__("mov $0, %rax; int $0x80");
-    while (1);
+    while (1) {
+        __asm__ __volatile__(
+                "\
+                mov $0x1, %rax; \
+                mov $0x0, %rbx; \
+                mov $0x0, %rcx; \
+                int $0x80;       \
+                shr $24, %rbx; \
+                cmpq $0, %rbx; \
+                je .done;        \
+                mov $0x0, %rax; \
+                int $0x80;      \
+                .done:");
+    }
     return 0;
 }
