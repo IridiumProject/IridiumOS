7fa891372801faf9c9509584c8b297a459892da9
Ian Moffett <spurious.vector@gmail.com>
2022-09-03 01:40:48 -0400

    Init: Do checking for init.sys

--- kernel/src/common/init.c
+++ kernel/src/common/init.c
@@ -1,6 +1,10 @@
 #include <common/init.h>
 #include <common/elf.h>
+#include <common/panic.h>
+#include <common/log.h>
+#include <common/asm.h>
 #include <proc/ring3.h>
+#include <fs/initrd.h>
 #include <stdint.h>
 #include <stddef.h>
 
@@ -8,6 +12,12 @@
 void start_init_system(void) {
     size_t unused;
     char* start_code = elf_get_entry("initrd/init.sys", &unused);
+
+    if (start_code == NULL) {
+        kprintf(KERN_PANIC "init.sys not found!\n");
+        CLI; HLT;
+    }
+
     void(*entry)(void) = (void*)start_code;
     switch_to_ring3(entry);
 }
