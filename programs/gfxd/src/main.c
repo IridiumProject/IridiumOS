400edc23581638e90e6077e53ef35bc92946af30
Ian Moffett <spurious.vector@gmail.com>
2022-09-04 02:22:02 -0400

    Programs: update.

--- programs/gfxd/src/main.c
+++ programs/gfxd/src/main.c
@@ -1,4 +1,26 @@
+#include <sys/drvctl.h>
+#include <sys/sysreq.h>
+
+
+static void clear_screen(void) {
+}
+
+
 int main(void) {
+    ERRNO_T errno;
+    /*
+     *  We, the display daemon have the PPERM_DRVCLAIM permission
+     *  thus we can claim the display driver.
+     *
+     *  Attempt to claim the display driver and clear screen.
+     *  TODO: Handle errors.
+     *
+     */
+    if ((errno = claimdrv(DRIVERTYPE_DISPLAY)) == EXIT_SUCCESS) {  
+        sysreq(UAPI_SCREEN_SERVICE, 0x0, NULL);
+        sysreq(UAPI_SCREEN_SERVICE, 0x090909, NULL);
+    }
+
     while (1);
     return 0;
 }
--- programs/init/src/main.c
+++ programs/init/src/main.c
@@ -1,31 +1,14 @@
 #include <stdint.h>
 
-
-
-
 int main(void) {
     const char* const DISPLAY_DAEMON = "initrd/gfxd.sys";
     __asm__ __volatile__(
-            "\
-            mov $0x3, %%rax; \
-            mov %0, %%rbx;  \
-            mov $0x0, %%rcx; \
+            "                                            \
+            mov $0x3, %%rax;                             \
+            mov %0, %%rbx;                               \
+            mov $1 << 1, %%rcx; /* PPERM_DRVCLAIM */     \
             int $0x80" :: "m" (DISPLAY_DAEMON));
 
-    while (1) {
-        __asm__ __volatile__(
-                "\
-                mov $0x1, %rax; \
-                mov $0x0, %rbx; \
-                mov $0x0, %rcx; \
-                int $0x80;       \
-                andq $1 << 24, %rbx; \
-                jz .done;             \
-                mov $0x0, %rax; \
-                int $0x80;      \
-                .done:");
-    }
-    
-    while (1);
+    while (1); 
     return 0;
 }
