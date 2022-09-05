52c4dceddbe3704c3c6106dbe34bcb50aaa8460a
Ian Moffett <spurious.vector@gmail.com>
2022-09-04 23:48:39 -0400

    Refactor: cleanup.

--- programs/init/src/main.c
+++ programs/init/src/main.c
@@ -1,13 +1,10 @@
 #include <stdint.h>
+#include <service.h>
+#include <perm.h>
 
 int main(void) {
     const char* const DISPLAY_DAEMON = "initrd/gfxd.sys";
-    __asm__ __volatile__(
-            "                                            \
-            mov $0x3, %%rax;                             \
-            mov %0, %%rbx;                               \
-            mov $1 << 1, %%rcx; /* PPERM_DRVCLAIM */     \
-            int $0x80" :: "m" (DISPLAY_DAEMON));
+    _service_create(DISPLAY_DAEMON, "gfxd", "Graphics daemon", PPERM_DRVCLAIM);
 
     while (1); 
     return 0;
