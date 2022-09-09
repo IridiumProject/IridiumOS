6fbe7f08f16a12e64bc77faf323764210689d231
Ian Moffett <spurious.vector@gmail.com>
2022-09-09 02:44:55 -0400

    Feat: console.

--- programs/sys/init/src/main.c
+++ programs/sys/init/src/main.c
@@ -1,16 +1,11 @@
 #include <stdint.h>
 #include <service.h>
-#include <perm.h>
-#include <sys/process.h>
-#include <gfx/draw.h>
+#include <sys/console.h>
 
-int main(void) {
-    const char* const DISPLAY_DAEMON = "initrd/gfxd.sys"; 
-    _service_create(DISPLAY_DAEMON, "gfxd", "Graphics daemon", PPERM_DRVCLAIM);
-
-    // Clear screen to remove boot logs.
-    libgfx_clearscreen(0x090909);
-
+void main(void) {
+    con_out_u32(CONSOLE_CLEAR, 0);
+    con_out_u32(CONSOLE_PURPLE "<==== IridiumD (init.sys) v0.0.1 ====>\n\n", 0);
+    con_out_u32(CONSOLE_BOLD_YELLOW "Starting shell.. (shell.sys)\n\n", 0);
+    _service_create("initrd/shell.sys", "Shell", "A system shell", PPERM_CONSOLE);
     while (1); 
-    return 0;
 }
