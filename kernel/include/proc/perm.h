41e7e124c252f3ff31e487ab254b9c1d4fd061ae
Ian Moffett <spurious.vector@gmail.com>
2022-09-09 02:38:20 -0400

    Proc: Add console permissions.

--- kernel/include/proc/perm.h
+++ kernel/include/proc/perm.h
@@ -14,4 +14,13 @@
  */
 #define PPERM_DRVCLAIM  (1 << 1)
 
+/*
+ *  Allows a process to 
+ *  write to the console.
+ */
+
+#define PPERM_CONSOLE   (1 << 2)
+
+#define PPERM_ADMIN     PPERM_CONSOLE           // Gives more permissions that a process usually lacks (Careful!).
+#define PPERM_SUPER_ADMIN     PPERM_CONSOLE | PPERM_DRVCLAIM | PPERM_PERM         // Gives all permissions (CAREFUL!!!!).
 #endif
