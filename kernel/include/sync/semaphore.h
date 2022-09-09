64835f8c0a05de09767d083a0a66d3969a729143
Ian Moffett <spurious.vector@gmail.com>
2022-09-09 02:40:02 -0400

    Feat: allow semaphore rejection.

--- kernel/include/sync/semaphore.h
+++ kernel/include/sync/semaphore.h
@@ -2,27 +2,47 @@
 #define SYNC_SEMAPHORE_H
 
 #include <sync/spinlock.h>
+#include <common/debug.h>
 #include <stddef.h>
 
 
-#define SEMAPHORE_INIT(max_n_cnt)    \
-    {                                \
-        .n = max_n_cnt,              \
-        .max_n = max_n_cnt,          \
-        .lock = 0                    \
+#define SEMAPHORE_INIT(sem_type, max_n_cnt)    \
+    {                                          \
+        .n = max_n_cnt,                        \
+        .max_n = max_n_cnt,                    \
+        .lock = 0,                             \
+        .sem_type = type                       \
     };
 
 
-#define SEMAPHORE_WRITE_INIT(max_n_cnt, sem)    \
-    sem.n = max_n_cnt;                          \
-    sem.max_n = max_n_cnt;                      \
-    sem.lock = 0;
+#define SEMAPHORE_WRITE_INIT(sem_type, max_n_cnt, sem)    \
+    sem.n = max_n_cnt;                                    \
+    sem.max_n = max_n_cnt;                                \
+    sem.lock = 0;                                         \
+    sem.type = sem_type;
+
+
+// !!!NOTE!!!: USE IF SEMAPHORE TYPE IS SEMAPHORE_REJECT.
+#define REJ_SEMAPHORE_DOWN(semptr)                  \
+    ASSERT((semptr)->type == SEMAPHORE_REJECT);     \
+    if ((semptr)->n > 0) {                          \
+        --(semptr)->n;                              \
+    } else {                                        \
+       return -EREJECTED;                           \
+    }
+
+
+typedef enum {
+    SEMAPHORE_LOCK,    // Locks if n == 0.
+    SEMAPHORE_REJECT,  // Function must have return type of ERRNO_T (returns error if n == 0).
+} SEMAPHORE_TYPE_T;
 
 
 typedef struct {
     _Atomic size_t n;
     size_t max_n;
     SPINLOCK_T lock;
+    SEMAPHORE_TYPE_T type;
 } SEMAPHORE_T;
 
 
--- kernel/src/sync/semaphore.c
+++ kernel/src/sync/semaphore.c
@@ -5,12 +5,13 @@ void semaphore_up(SEMAPHORE_T* sem) {
         ++sem->n;
     }
 
-    if (sem->n == 1) {
+    if (sem->n == 1 && sem->type == SEMAPHORE_LOCK) {
         spinlock_release(&sem->lock);
     }
 }
 
 void semaphore_down(SEMAPHORE_T* sem) {
+    ASSERT(sem->type == SEMAPHORE_LOCK);    // Must be a lock-type semaphore.
     if (sem->n > 0) {
         --sem->n;
     } else {
