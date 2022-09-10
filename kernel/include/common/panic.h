742da8042805fd3e70e691a6916be40c2b1e2fd5
Ian Moffett <spurious.vector@gmail.com>
2022-09-09 23:55:07 -0400

    Misc: update panic.h

--- kernel/include/common/panic.h
+++ kernel/include/common/panic.h
@@ -1,7 +1,12 @@
 #ifndef PANIC_H
 #define PANIC_H
 
+#include <common/log.h>
+
+
 #define KERN_PANIC "\033[H\033[2J\033[3J\e[0;31m**KERNEL PANIC**\n"
+#define KPANIC_NOCLEAR "\e[0;31m**KERNEL PANIC**\n"
+#define PPANIC_NOCLEAR "\n\n\e[0;31mProgram terminated: " WHITE        // Program panic.
 
 // Called by exceptions.asm
 void print_panic_msg(void);
