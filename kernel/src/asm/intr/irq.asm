27f8e6754508facebfb39c719fe80687a4067777
Ian Moffett <spurious.vector@gmail.com>
2022-09-09 23:58:50 -0400

    Feat: change tick var size.

--- kernel/src/asm/intr/irq.asm
+++ kernel/src/asm/intr/irq.asm
@@ -7,10 +7,10 @@ extern switch_task
 
 irq0_isr:
 	cli
-	inc word [pit_ticks]
+	inc dword [pit_ticks]
 	call lapic_send_eoi
     jmp switch_task
     ;; --- NO RETURN ---
 
 section .data
-pit_ticks: dw 0
+pit_ticks: dd 0
