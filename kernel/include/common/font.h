0d1a5fd2490a8248a762b7333966b4eb159d44cd
Ian Moffett <spurious.vector@gmail.com>
2022-09-11 20:20:01 -0400

    Misc: Update font.c

--- kernel/include/common/font.h
+++ kernel/include/common/font.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 
 
-void write_string(uint32_t x, uint32_t y, const char* string, uint32_t color);
-
+void write_string(const char* string, uint32_t color);
+void init_font(void);
 
 
 #endif
--- kernel/src/common/font.c
+++ kernel/src/common/font.c
@@ -8,6 +8,9 @@
 #include <drivers/video/lfb.h>
 #include <stdint.h>
 
+#define PSF1_MAGIC0 0x36
+#define PSF1_MAGIC1 0x04
+
 static uint8_t init = 0;
 
 static struct FontHeader {
@@ -18,7 +21,7 @@ static struct FontHeader {
 
 static void* font = NULL;
 
-static void init_font(void) {
+void init_font(void) {
     int8_t* font_data = (int8_t*)initrd_open("initrd/font.psf");
 
     if (font_data == NULL) {
@@ -39,6 +42,11 @@ static void init_font(void) {
         header_ptr[i] = font_data[i];
     }
 
+    if (font_header->magic[0] != PSF1_MAGIC0 && font_header->magic[1] != PSF1_MAGIC1) {
+        kprintf(KERN_PANIC "Builtin font magic invalid!\n");
+        CLI; HLT;
+    }
+
     uint64_t glyph_buffer_size = font_header->mode == 1 ? font_header->chsize * 512 : font_header->chsize * 256;
     font = kmalloc(glyph_buffer_size);
 
@@ -51,8 +59,6 @@ static void init_font(void) {
     for (uint64_t i = sizeof(struct FontHeader), j = 0; i < sizeof(struct FontHeader) + glyph_buffer_size; ++i, ++j) {
         glyph_buffer_ptr[j] = font_data[i];
     }
-
-    init = 1;
 }
 
 
@@ -60,9 +66,8 @@ void putchar(uint32_t x, uint32_t y, uint32_t color, char c) {
     char* fontPtr = (char*)(font + (c * font_header->chsize));
     for (uint64_t cy = y; cy < y + 16; ++cy) {
         for (uint64_t cx = x; cx < x + 8; ++cx) {
-            if (cy < y+2) continue;     // To remove artifacts on top of letter.
-
-            if ((*fontPtr & ((1 << 7) >> (cx - x))) > 0) {
+            // if (cy < y+2) continue;     // To remove artifacts on top of letter.
+            if ((*fontPtr & (0b10000000 >> (cx - x))) > 0) {
                 lfb_put_pixel(cx, cy, color);
             }
 
@@ -73,13 +78,29 @@ void putchar(uint32_t x, uint32_t y, uint32_t color, char c) {
 }
 
 
-void write_string(uint32_t x, uint32_t y, const char* string, uint32_t color) {
-    if (!(init)) {
-        init_font();
-    }
+void write_string(const char* string, uint32_t color) {
+    static uint32_t x = 0;
+    static uint32_t y = 0;
+    const uint32_t X_INC = 8;
+    const uint32_t Y_INC = 10; 
 
     for (int i = 0; i < strlen(string); ++i) {
+        if (string[i] == '\n') {
+            y += Y_INC;
+            x = 0;
+            continue;
+        }
+
         putchar(x, y, color, string[i]);
-        x += 8;
+        x += X_INC;
+
+        if (x >= lfb_get_width() - 1) {
+            y += Y_INC;
+            x = 0;
+        }
+
+        if (x >= lfb_get_height() - 1) {
+            lfb_clear_screen(0x000000);
+        }
     }
 }
