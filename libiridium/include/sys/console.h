112ff10f687bd7d6dafb5ef926815cf1392d6ac0
Ian Moffett <spurious.vector@gmail.com>
2022-09-11 20:35:45 -0400

    Misc: update print function.

--- libiridium/include/sys/console.h
+++ libiridium/include/sys/console.h
@@ -5,16 +5,12 @@
 #include <errno.h>
 #include <stdint.h>
 
-#define CONSOLE_BLUE "\e[0;34m"
-#define CONSOLE_YELLOW "\e[0;33m"
-#define CONSOLE_PURPLE "\e[0;35m"
-#define CONSOLE_CYAN "\e[0;36m"
-#define CONSOLE_WHITE "\e[0;37m"
-#define CONSOLE_BOLD_YELLOW "\e[1;33m"
-#define CONSOLE_CLEAR "\033[H\033[2J\033[3J"
+
+#define RGB2HEX(r, g, b) (r << 16 | g << 8 | b)
+#define DEFAULT_COLOR RGB2HEX(255, 255, 0)
 
 
-_syscallab ERRNO_T con_out_u32(const char* str, uint32_t arg);
+_syscallab ERRNO_T con_out_u32(const char* str, uint32_t color);
 
 
 #endif
