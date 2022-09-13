deabb7c085da31fb07a6a22831e8f9ab96cc8bc9
Ian Moffett <spurious.vector@gmail.com>
2022-09-13 14:43:22 -0400

    Feat: notify shell.sys of keystrokes.

--- programs/sys/init/src/main.c
+++ programs/sys/init/src/main.c
@@ -4,16 +4,22 @@
 #include <sys/console.h>
 #include <sys/process.h>
 #include <sys/sysreq.h>
-#include <sys/key.h>
+#include <sys/initrd.h>
 #include <unistd.h>
 
 static uint8_t psignal_lock = MUTEX_UNLOCKED;
+static PID_T shell_pid;
 
 static void psignal_callback(void) {
     mutex_acquire(&psignal_lock);
     PSIGNAL_T psignal = psignal_fetch();
-    char key_char[2] = {KEYSTROKE_EXTRACT_CHARACTER(sysreq(UAPI_KEYB_SERVICE, 0)), 0};
-    con_out_u32(key_char, DEFAULT_COLOR);
+    
+    if (PSIGNAL_EXTRACT_SIGPAYLOAD(psignal) == PSIGNAL_KEYSTROKE) {
+        // Tell shell.sys about keystroke.
+        psignal_send(shell_pid, PSIGNAL_EXTRACT_SIGPAYLOAD(psignal));
+    }
+
+    // Done.
     mutex_release(&psignal_lock);
     exit();
 }
@@ -22,7 +28,8 @@ static void psignal_callback(void) {
 void main(void) {
     con_out_u32("<==== IridiumD (init.sys) v0.0.1 ====>\n\n", RGB2HEX(138, 43, 226));
     con_out_u32("Starting shell.. (shell.sys)\n\n", DEFAULT_COLOR);
-    // _service_create("initrd/shell.sys", "Shell", "A system shell", PPERM_CONSOLE);
     psignal_hook(psignal_callback);
+    shell_pid = _service_create("initrd/shell.sys", "Shell", "A system shell", PPERM_CONSOLE, 1);
+
     while (1); 
 }
