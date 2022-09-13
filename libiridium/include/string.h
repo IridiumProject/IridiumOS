f999bb911eb1160f04668767944b89b4d101ba44
Ian Moffett <spurious.vector@gmail.com>
2022-09-13 18:06:54 -0400

    Misc: update string lib.

--- libiridium/include/string.h
+++ libiridium/include/string.h
@@ -2,9 +2,11 @@
 #define LIBIRIDIUM__STRING_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 
 size_t strlen(const char* str);
+uint8_t strncmp(const char* str1, const char* str2, size_t n);             // Returns 0 if equal otherwise 1.
 
 
 #endif
--- libiridium/src/string.c
+++ libiridium/src/string.c
@@ -6,3 +6,14 @@ size_t strlen(const char* str) {
     while (str[i++]);
     return i - 1;
 }
+
+
+uint8_t strncmp(const char* str1, const char* str2, size_t n) { 
+    for (size_t i = 0; i < n; ++i) {
+        if (str1[i] != str2[i]) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
