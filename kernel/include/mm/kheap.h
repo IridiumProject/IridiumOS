f490501c0a96c3638c7f5f79e46e2ee63c612bfe
Ian Moffett <spurious.vector@gmail.com>
2022-09-13 14:36:15 -0400

    Cleanup: remove kheap status.

--- kernel/include/mm/kheap.h
+++ kernel/include/mm/kheap.h
@@ -3,31 +3,10 @@
 
 #include <stddef.h>
 
-
-typedef size_t KHEAP_STATUS_T;
-
-
-typedef enum {
-  KHEAP_INVALID_FREE = (1 << 0),
-  KHEAP_NO_MEMORY = (1 << 1)
-} KHEAP_ERR;
-
-
 void kheap_init(void);
 void* kmalloc(size_t n_bytes);
 void* krealloc(void* old, size_t n_bytes);
 void* kmalloc_user(size_t n_bytes);
 void kfree(void* block);
-
-
-/*
- *  Mostly used for debugging,
- *  returns a status code on how
- *  a heap operation went.
- *
- */
-
-KHEAP_STATUS_T kheap_status(void);
-
 
 #endif
