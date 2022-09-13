1eb422aeb8a7da2f9df526d8d6f4d6d32b5ca87c
Ian Moffett <spurious.vector@gmail.com>
2022-09-13 14:43:38 -0400

    Feat: Add uses_console param.

--- programs/sys/init/src/service.c
+++ programs/sys/init/src/service.c
@@ -6,12 +6,12 @@
 static struct Service services[MAX_SERVICES];
 
 
-PID_T _service_create(const char* path, const char* name, const char* description, PPERM_T pmask) {
+PID_T _service_create(const char* path, const char* name, const char* description, PPERM_T pmask, uint8_t uses_console) {
     static struct Service service;
     service.perm_mask = pmask;
     service.service_name = name;
     service.service_description = description; 
-    PID_T ret = service.pid = _initrd_spawn(path, pmask, 0);
+    PID_T ret = service.pid = _initrd_spawn(path, pmask, uses_console);
     services[service.pid] = service;
     return ret;
 }
